dc633c505c262d7d23c18e006eb9c95d4f032af8
Ruan Gomes <ruan.alves@ccc.ufcg.edu.br>
2022-03-18 22:54:06 -0300

    add exception handler

--- src/app/utils/decorators.py
+++ src/app/utils/decorators.py
@@ -1,7 +1,10 @@
 from app.models import user as user_model
 from app.utils import jwt_utils
+from app.utils import response_factory
 from functools import wraps
+from app.utils import exceptions
 from flask import request
+
 
 def check_authorization(func):
     @wraps(func)
@@ -13,10 +16,24 @@ def check_authorization(func):
 
             user = user_model.search(payload.get('username'))
             if not (user and jwt_utils.validate_token(token, user)):
-                raise Exception()
+                raise exceptions.InvalidTokenException()
         else:
-            raise Exception()
-        
+            raise exceptions.InvalidAuthorizationHeader()
+
         return func(*args, **kwargs)
+
+    return wrap
+
+
+def exception_handler(func):
+    @wraps(func)
+    def wrap(*args, **kwargs):
+        try:
+            return func(*args, **kwargs)
+        except Exception as exception:
+            return response_factory.make_error(
+                http_code=exception.status_code,
+                message=str(exception)
+            )
 
     return wrap
